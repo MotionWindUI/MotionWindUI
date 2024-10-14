3492ea36ce2d990ea3a0b4d1e9897b67d000df3c
Kyle Gagnon <kmgagnon99@gmail.com>
2024-10-14 19:29:16 -0400

    chore: update eslint

--- packages/core/core/eslint.config.js
+++ packages/core/core/eslint.config.js
@@ -1,3 +1,9 @@
 import libConfig from '@motionwindui/eslint-config/library';
 
-export default libConfig;
+export default [
+  ...libConfig,
+  {
+    ignores: ['*.config.*', '*.d.ts', 'dist/', 'node_modules/'],
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+  },
+];
